5e0a9296f390d6a1cb873ede2b8b3be032903235
Pranat Praveer <praveer.pranat2@gmail.com>
2023-05-23 17:38:12 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// This function scrolls the page to the top.
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+// This function is called when the user clicks on the "Contact" link.
+function contact() {
+  window.location.href = "mailto:bard@example.com";
+}
